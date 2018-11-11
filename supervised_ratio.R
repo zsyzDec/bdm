5f34b59fc36419c3a5b5e03d21f55239582ce29a
Allen-Qiu <jiangtaoqiu@gmail.com>
2018-11-11 11:30:28 +0800

    Add files via upload

--- supervised_ratio.R
+++ supervised_ratio.R
@@ -1,4 +1,4 @@
-# The functions for supervised ratio and WoE
+# A function for supervised ration
 # allen -- 2018.11
 # 
 # data is a data frame
@@ -22,16 +22,17 @@ supervised_ratio <- function(dat, dependent, independent, positive){
 }
 
 woe<-function(dat, dependent, independent, positive){
-    TC<-length(dat[,dependent]==positive)
-    TN<-length(dat[,dependent]!=positive)
+    TC<-length(dat[,dependent]==positive)+1
+    TN<-length(dat[,dependent]!=positive)+1
     tab<-aggregate(x=rep(1, nrow(dat)), 
                    by=list(dat[,independent],dat[,dependent]),
                    FUN=sum)
     r<-sapply(X=dat[,independent], FUN=function(x){
         # browser()
-        ci <- as.numeric(tab[tab$Group.1==x & tab$Group.2==positive,][3])
-        ni <- as.numeric(tab[tab$Group.1==x & tab$Group.2!=positive,][3])
-        return (log((ci*TN)/(ni*TC)))
+        ci <- as.numeric(tab[tab$Group.1==x & tab$Group.2==positive,][3])+1
+        ni <- as.numeric(tab[tab$Group.1==x & tab$Group.2!=positive,][3])+1
+        res <- log((ci*TN)/(ni*TC))
+        return (ifelse(is.na(res),0,res))
     })
     return(unlist(r))
 }
